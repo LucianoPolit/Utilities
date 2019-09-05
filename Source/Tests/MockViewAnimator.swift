1a8b3bc4229711ca80d41a4673fa9c907089a533
Luciano Polit <lucianopolit@gmail.com>
2019-09-05 12:16:06 +0800

    Update tests

--- Source/Tests/MockViewAnimator.swift
+++ Source/Tests/MockViewAnimator.swift
@@ -12,25 +12,19 @@ public class MockViewAnimator: ViewAnimator {
     public var animations: [MockAnimation] = []
     
     public override func begin() -> Animation {
-        let animation = MockAnimation().begin()
+        let animation = MockAnimation()
         animations.append(animation)
         return animation
     }
     
     public class MockAnimation: Animation {
         
-        public private(set) var beginCalled = false
         public private(set) var duration: TimeInterval?
         public private(set) var delay: TimeInterval?
         public private(set) var options: UIView.AnimationOptions?
         public private(set) var animations: (() -> ())?
         public private(set) var completion: ((Bool) -> ())?
         public private(set) var commitCalled = false
-        
-        public override func begin() -> Self {
-            beginCalled = true
-            return self
-        }
         
         public override func duration(_ duration: TimeInterval) -> Self {
             self.duration = duration
